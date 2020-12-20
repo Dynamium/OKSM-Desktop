3ce17f91f13e0897ea445256ebec7eb1fa7a7b06
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-20 11:34:38 +0200

    add some more icons

---
+++ src/main/kotlin/org/dynamium/oksm/ui/icons/Redo.kt
@@ -0,0 +1,9 @@
+package org.dynamium.oksm.ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorXmlResource
+
+val Icons.Outlined.Redo: ImageVector
+    @Composable get() = vectorXmlResource("drawables/ic_redo_24px.xml")
---
+++ src/main/kotlin/org/dynamium/oksm/ui/icons/Undo.kt
@@ -0,0 +1,9 @@
+package org.dynamium.oksm.ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorXmlResource
+
+val Icons.Outlined.Undo: ImageVector
+    @Composable get() = vectorXmlResource("drawables/ic_undo_24px.xml")
