7384a028b0e5d30ad581218783926ba5f5e12eeb
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-21 13:48:47 +0200

    updated main screen

--- src/main/kotlin/org/dynamium/oksm/ui/screens/MainScreen.kt
+++ src/main/kotlin/org/dynamium/oksm/ui/screens/MainScreen.kt
@@ -9,21 +9,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.dynamium.oksm.ui.components.Sidebar
 import org.dynamium.oksm.ui.components.editor.Editor
-import org.dynamium.oksm.ui.components.editor.TopBar
+import org.dynamium.oksm.ui.components.editor.Topbar
 
 @Composable
 fun MainScreen() {
     Column {
-        TopBar()
+        Topbar()
         Row(
             modifier = Modifier
                 .fillMaxSize(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Editor()
-            Sidebar()
         }
     }
 }
