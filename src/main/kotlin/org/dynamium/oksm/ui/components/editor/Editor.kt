13bca3721230f186667b3db592c761b2b0c724f0
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-21 13:48:29 +0200

    moved sidebar to the editor

--- src/main/kotlin/org/dynamium/oksm/ui/components/editor/Editor.kt
+++ src/main/kotlin/org/dynamium/oksm/ui/components/editor/Editor.kt
@@ -3,12 +3,15 @@
 package org.dynamium.oksm.ui.components.editor
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 
 
 @Composable fun Editor() {
@@ -17,8 +20,17 @@ import androidx.compose.ui.graphics.Color
             .fillMaxSize(),
         color = Color.White
     ) {
-        Column {
-            Text("Test")
+        Row {
+            // Editor
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                val searchBoxText = remember { mutableStateOf(TextFieldValue("")) }
+            }
+
+            // Sidebar
+            Sidebar()
         }
     }
 }
