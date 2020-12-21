1c6386a857b0a8f73b0045dca1f85f046d7d7337
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-21 13:45:57 +0200

    add a new icon

---
+++ src/main/kotlin/org/dynamium/oksm/ui/icons/Backspace.kt
@@ -0,0 +1,9 @@
+package org.dynamium.oksm.ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorXmlResource
+
+val Icons.Outlined.Backspace: ImageVector
+    @Composable get() = vectorXmlResource("drawables/ic_backspace_24px.xml")
