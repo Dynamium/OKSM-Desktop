e49cca1238447f09a9ff8e239590d4b1cad6815d
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-25 11:50:25 +0200

    Update sidebar so it now contains the previously mentioned Inspector

--- src/main/kotlin/org/dynamium/oksm/ui/components/editor/sidebar/Sidebar.kt
+++ src/main/kotlin/org/dynamium/oksm/ui/components/editor/sidebar/Sidebar.kt
@@ -1,28 +1,32 @@
 @file:Suppress("FunctionName")
 
-package org.dynamium.oksm.ui.components.editor
+package org.dynamium.oksm.ui.components.editor.sidebar
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@Composable fun Sidebar() {
+@Composable
+fun Sidebar() {
     Surface(
         modifier = Modifier
             .fillMaxHeight()
-            .width(512.dp)
+            .width((512 + 128).dp)
             .shadow(8.dp),
         color = Color.White
     ) {
-        Column {
-            Text("Test")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Inspector()
         }
     }
 }
